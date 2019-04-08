2680b3e7450aec6f4c90f6f0ce34c7188b1d98f5
Sumitkumar Rakshit <SumitR@talentica-all.com>
2019-04-08 15:59:48 +0530

    Updates

---
+++ precache-manifest.d918b944fcd38540e3a5df11c24538c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-userlist-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c22dc309975967fc141d",
+    "url": "/react-userlist-app/static/js/runtime~main.8f60c97f.js"
+  },
+  {
+    "revision": "5151199569ce98bde63a",
+    "url": "/react-userlist-app/static/js/main.29ede0f3.chunk.js"
+  },
+  {
+    "revision": "057b8f35c6ba80c8cfbf",
+    "url": "/react-userlist-app/static/js/2.10170861.chunk.js"
+  },
+  {
+    "revision": "5151199569ce98bde63a",
+    "url": "/react-userlist-app/static/css/main.5b8c308c.chunk.css"
+  },
+  {
+    "revision": "c637ef81a385bbb1f01fffcfbc4da5ff",
+    "url": "/react-userlist-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-userlist-app/precache-manifest.d918b944fcd38540e3a5df11c24538c6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-userlist-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
